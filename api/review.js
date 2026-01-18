415bd5cdd02cf18b3612794000d1ed795ac8deb3
Islam Badran <islambadran39@gmail.com>
2026-01-18 18:26:06 +0200

    update api

--- api/review.js
+++ api/review.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const upstream = await fetch("http://62.72.22.223/api/review/", {
+    const upstream = await fetch("http://62.72.22.223/news_reviewer/api/review/", {
       method: req.method,
       headers: { "Content-Type": "application/json" },
       body: req.method === "GET" ? undefined : JSON.stringify(req.body),
