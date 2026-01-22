191a27f5411ab3ea8ac647a5a9975838913a1205
Islam Badran <islambadran39@gmail.com>
2026-01-22 05:52:50 +0200

    update request

--- api/review.js
+++ api/review.js
@@ -2,7 +2,11 @@ export default async function handler(req, res) {
   try {
     const upstream = await fetch("http://62.72.22.223/news_reviewer/api/review/", {
       method: req.method,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "nra_abeffba708601616e0a1ecd9f93387e0db8d7d3a19aa3d513fc9b849",
+        ...(req.headers.authorization && { Authorization: req.headers.authorization }),
+      },
       body: req.method === "GET" ? undefined : JSON.stringify(req.body),
     });
 
